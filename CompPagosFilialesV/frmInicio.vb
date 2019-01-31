814be25ebc7193723127a6a34e20dc174657d892
viapolo <viapolo@finagil.com.mx>
2019-01-31 16:22:40 -0600

    - Complemento de pago sin UUID

--- CompPagosFilialesV/frmInicio.vb
+++ CompPagosFilialesV/frmInicio.vb
@@ -166,9 +166,17 @@ Public Class frmPrincipal
 
     Private Sub cmbRFC_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbRFC.SelectedIndexChanged
         Me.taCFDI_CompConsFactoraje.NProcXRFC_FillBy(Me.dsProduction.CFDI_CompConsFactoraje, cmbRFC.Text)
+
         For i = 0 To dgvComplementos.Rows.Count - 1
+            If cmbRFC.Text = "GTC980421R4A" Then
+                Me.dgvComplementos.Item("ProcesadoDataGridViewCheckBoxColumn", i).ReadOnly = False
+            End If
             Me.dgvComplementos.Item("dgDocRelacionado", i).Value = "UUID's Rel"
         Next
+        If cmbRFC.Text = "GTC980421R4A" Then
+            btnProcesar.Enabled = False
+            btnTimbrar.Enabled = True
+        End If
     End Sub
 
     Public Function regresaSF(ByVal Referencia As String)
@@ -281,7 +289,7 @@ Public Class frmPrincipal
                 End If
                 Me.Refresh()
                 pbTodos.Value += 1
-            Else
+            ElseIf dgvComplementos.Item("RFCDataGridViewTextBoxColumn", i).Value = "GTC980421R4A" Then
                 'NO FILIALES
                 Me.taComplementos.DocRela_FillBy(Me.dsComplemento.Vw_ChequesDetalle, dgvComplementos.Item("RFCDataGridViewTextBoxColumn", i).Value, dgvComplementos.Item("ChequeDataGridViewTextBoxColumn", i).Value)
                 Dim importeXML As Double = 0
@@ -302,17 +310,17 @@ Public Class frmPrincipal
                     pbCheque.Value += 1
                 Next
 
-                If Math.Round(importeXML, 2) = dgvComplementos.Item("MontoDataGridViewTextBoxColumn", i).Value Then
-                    Me.dgvComplementos.Item("MontoDataGridViewTextBoxColumn", i).Style.BackColor = Color.Green
+                'If Math.Round(importeXML, 2) = dgvComplementos.Item("MontoDataGridViewTextBoxColumn", i).Value Then
+                Me.dgvComplementos.Item("MontoDataGridViewTextBoxColumn", i).Style.BackColor = Color.Green
                     Me.dgvComplementos.Item("ProcesadoDataGridViewCheckBoxColumn", i).ReadOnly = False
                     verdes += 1
-                Else
-                    Me.dgvComplementos.Item("MontoDataGridViewTextBoxColumn", i).Style.BackColor = Color.Firebrick
-                    Me.dgvComplementos.Item("ProcesadoDataGridViewCheckBoxColumn", i).ReadOnly = True
-                End If
+                'Else
+                'Me.dgvComplementos.Item("MontoDataGridViewTextBoxColumn", i).Style.BackColor = Color.Green
+                'Me.dgvComplementos.Item("ProcesadoDataGridViewCheckBoxColumn", i).ReadOnly = False
+                'End If
                 Me.Refresh()
-                pbTodos.Value += 1
-            End If
+                    pbTodos.Value += 1
+                End If
         Next
         pbCheque.Visible = False
         pbTodos.Visible = False
@@ -599,8 +607,181 @@ Public Class frmPrincipal
                     Next
                     taConsecutivo.ActAProcesado(rowCFDI_ENC._27_Serie_Comprobante, rowCFDI_ENC._1_Folio, varCheque, rowCFDI_ENC._43_RFC_Receptor)
                 End If
-            Else
+            ElseIf dgvComplementos.Item("RFCDataGridViewTextBoxColumn", i).Value = "GTC980421R4A" Then
                 Dim valor As Boolean = Me.dgvComplementos.Item("ProcesadoDataGridViewCheckBoxColumn", i).Value
+                If Me.dgvComplementos.Item("ProcesadoDataGridViewCheckBoxColumn", i).Value = True Then
+                    varFechaDePago = dgvComplementos.Item("FechaPagoDataGridViewTextBoxColumn", i).Value
+                    varFormaDePago = dgvComplementos.Item("FormaDePagoPDataGridViewTextBoxColumn", i).Value
+                    varMoneda = dgvComplementos.Item("MonedaPDataGridViewTextBoxColumn", i).Value
+                    varTipoDeCambio = ""
+                    varMontoDePago = dgvComplementos.Item("MontoDataGridViewTextBoxColumn", i).Value
+                    varReferencia = dgvComplementos.Item("NumOperacionDataGridViewTextBoxColumn", i).Value + ""
+                    varRFCBancoOrdenante = dgvComplementos.Item("RFCEmisorCtaOrdDataGridViewTextBoxColumn", i).Value + ""
+                    varNombreBancoOrdenante = dgvComplementos.Item("NomBancoOrdExtDataGridViewTextBoxColumn", i).Value + ""
+                    varCtaOrdenante = dgvComplementos.Item("CtaOrdenanteDataGridViewTextBoxColumn", i).Value + ""
+                    varCheque = dgvComplementos.Item("ChequeDataGridViewTextBoxColumn", i).Value
+
+                    Me.taComplementos.DocRela_FillBy(Me.dsComplemento.Vw_ChequesDetalle, dgvComplementos.Item("RFCDataGridViewTextBoxColumn", i).Value, dgvComplementos.Item("ChequeDataGridViewTextBoxColumn", i).Value)
+                    Dim rowsD3 As Factor100DataSet.Vw_ChequesDetalleRow
+                    Dim rowCFDI_ENC As Production_AUXDataSet.CFDI_EncabezadoRow
+                    rowCFDI_ENC = dsCFDI_ENC.CFDI_Encabezado.NewCFDI_EncabezadoRow()
+
+                    rowCFDI_ENC._1_Folio = taCFDI_ENC.ObtieneFolioFact()
+                    rowCFDI_ENC._2_Nombre_Emisor = "FINAGIL S.A. DE C.V, SOFOM E.N.R"
+                    rowCFDI_ENC._3_RFC_Emisor = "FIN940905AX7"
+                    rowCFDI_ENC._4_Dom_Emisor_calle = "Leandro Valle"
+                    rowCFDI_ENC._5_Dom_Emisor_noExterior = "402"
+                    rowCFDI_ENC._6_Dom_Emisor_noInterior = ""
+                    rowCFDI_ENC._7_Dom_Emisor_colonia = "Reforma y F.F.C.C"
+                    rowCFDI_ENC._8_Dom_Emisor_localidad = "Toluca"
+                    rowCFDI_ENC._9_Dom_Emisor_referencia = ""
+                    rowCFDI_ENC._10_Dom_Emisor_municipio = "Toluca"
+                    rowCFDI_ENC._11_Dom_Emisor_estado = "Estado de México"
+                    rowCFDI_ENC._12_Dom_Emisor_pais = "México"
+                    rowCFDI_ENC._13_Dom_Emisor_codigoPostal = "50070"
+                    rowCFDI_ENC._26_Version = "3.3"
+                    rowCFDI_ENC._27_Serie_Comprobante = "REPF"
+                    rowCFDI_ENC._29_FormaPago = ""
+                    rowCFDI_ENC._30_Fecha = Date.Now.ToString("yyyy-MM-dd")
+                    rowCFDI_ENC._31_Hora = Date.Now.ToString("hh:mm:ss")
+                    rowCFDI_ENC._41_Dom_LugarExpide_codigoPostal = "50070"
+                    rowCFDI_ENC._42_Nombre_Receptor = taClientes.ScalarQuery(dgvComplementos.Item("RFCDataGridViewTextBoxColumn", i).Value).ToString.Trim
+                    rowCFDI_ENC._43_RFC_Receptor = dgvComplementos.Item("RFCDataGridViewTextBoxColumn", i).Value
+                    rowCFDI_ENC._54_Monto_SubTotal = 0
+                    rowCFDI_ENC._55_Monto_IVA = 0
+                    rowCFDI_ENC._56_Monto_Total = 0
+                    rowCFDI_ENC._57_Estado = "1"
+                    rowCFDI_ENC._58_TipoCFD = "FA"
+                    rowCFDI_ENC._83_Cod_Moneda = "XXX"
+                    rowCFDI_ENC._97_Condiciones_Pago = ""
+                    rowCFDI_ENC._100_Letras_Monto_Total = ""
+                    rowCFDI_ENC._113_Misc01 = "[CPG_FINAGIL]"
+                    rowCFDI_ENC._114_Misc02 = ""
+                    rowCFDI_ENC._132_Misc20 = "[CPG]"
+                    rowCFDI_ENC._144_Misc32 = "P01"
+                    rowCFDI_ENC._157_Misc45 = ""
+                    rowCFDI_ENC._158_Misc46 = ""
+                    rowCFDI_ENC._159_Misc47 = ""
+                    rowCFDI_ENC._162_Misc50 = (taMailCFDI.ObtMail1(rowCFDI_ENC._43_RFC_Receptor) + ";" + taMailCFDI.ObtMail2(rowCFDI_ENC._43_RFC_Receptor) + ";" + taMailCFDI.ObtMail3(rowCFDI_ENC._43_RFC_Receptor) + ";" + taMailCFDI.ObtMail4(rowCFDI_ENC._43_RFC_Receptor)).ToString.Replace(";;", ";").Replace(";;;", ";")
+                    rowCFDI_ENC._167_RegimentFiscal = "601"
+                    rowCFDI_ENC._180_LugarExpedicion = "50070"
+                    rowCFDI_ENC._190_Metodo_Pago = ""
+                    rowCFDI_ENC._191_Efecto_Comprobante = "P"
+                    rowCFDI_ENC._192_Monto_TotalImp_Retenidos = 0
+                    rowCFDI_ENC._193_Monto_TotalImp_Trasladados = 0
+                    rowCFDI_ENC.Encabezado_Procesado = False
+                    taCFDI_ENC.ConsumeFolioFact()
+
+                    dsCFDI_ENC.CFDI_Encabezado.Rows.Add(rowCFDI_ENC)
+                    taCFDI_ENC.Update(dsCFDI_ENC.CFDI_Encabezado)
+
+                    'Inserta información en Tabla CFDI_Detalle
+                    Dim rowCFDI_DET As Production_AUXDataSet.CFDI_DetalleRow
+                    rowCFDI_DET = dsCFDI_DET.CFDI_Detalle.NewCFDI_DetalleRow()
+
+                    rowCFDI_DET._1_Linea_Descripcion = "Pago"
+                    rowCFDI_DET._2_Linea_Cantidad = 1
+                    rowCFDI_DET._3_Linea_Unidad = "ACT"
+                    rowCFDI_DET._4_Linea_PrecioUnitario = 0
+                    rowCFDI_DET._5_Linea_Importe = 0
+                    rowCFDI_DET._16_Linea_Cod_Articulo = "84111506"
+                    rowCFDI_DET.Detalle_Folio = rowCFDI_ENC._1_Folio
+                    rowCFDI_DET.Detalle_Serie = rowCFDI_ENC._27_Serie_Comprobante
+
+                    dsCFDI_DET.CFDI_Detalle.Rows.Add(rowCFDI_DET)
+                    taCFDI_DET.Update(dsCFDI_DET.CFDI_Detalle)
+
+                    'Inserta información en tabla CFDI_Complemento, sección Pagos
+
+                    Dim rowCFD_CPAGO As Production_AUXDataSet.CFDI_ComplementoPagoRow
+                    rowCFD_CPAGO = dsCFDI_CPAGO.CFDI_ComplementoPago.NewCFDI_ComplementoPagoRow()
+
+                    rowCFD_CPAGO._1_DetalleAux_Tipo = "CPG"
+                    rowCFD_CPAGO._2_DetalleAux_DescTipo = "Pagos"
+                    rowCFD_CPAGO._3_DetalleAux_Misc01 = "HD"
+                    rowCFD_CPAGO._4_DetalleAux_Misc02 = "1.0"
+                    rowCFD_CPAGO._5_DetalleAux_Misc03 = CDate(varFechaDePago).ToString("yyyy-MM-dd") + "T12:00:00"
+                    rowCFD_CPAGO._6_DetalleAux_Misc04 = varFormaDePago.Replace("Spei", "03")
+                    rowCFD_CPAGO._7_DetalleAux_Misc05 = varMoneda.Replace("PESO", "MXN")
+                    rowCFD_CPAGO._8_DetalleAux_Misc06 = ""
+                    rowCFD_CPAGO._9_DetalleAux_Misc07 = varMontoDePago
+                    rowCFD_CPAGO._10_DetalleAux_Misc08 = varReferencia
+                    rowCFD_CPAGO._11_DetalleAux_Misc09 = varRFCBancoOrdenante
+                    rowCFD_CPAGO._12_DetalleAux_Misc10 = validaNull(taCFDI_Bancos.ObtNomBanco(rowCFD_CPAGO._11_DetalleAux_Misc09))
+                    rowCFD_CPAGO._13_DetalleAux_Misc11 = ""
+                    rowCFD_CPAGO._18_DetalleAux_Misc16 = ""
+                    rowCFD_CPAGO._19_DetalleAux_Folio = rowCFDI_ENC._1_Folio
+                    rowCFD_CPAGO._20_DetalleAux_Serie = rowCFDI_ENC._27_Serie_Comprobante
+
+                    dsCFDI_CPAGO.CFDI_ComplementoPago.Rows.Add(rowCFD_CPAGO)
+                    taCFDI_CPAGO.Update(dsCFDI_CPAGO.CFDI_ComplementoPago)
+
+                    'Inserta información en tabla CFDI_Complemento, sección Pago
+
+                    rowCFD_CPAGO = dsCFDI_CPAGO.CFDI_ComplementoPago.NewCFDI_ComplementoPagoRow()
+
+                    rowCFD_CPAGO._1_DetalleAux_Tipo = "CPG"
+                    rowCFD_CPAGO._2_DetalleAux_DescTipo = "Pago"
+                    rowCFD_CPAGO._3_DetalleAux_Misc01 = "HD"
+                    rowCFD_CPAGO._4_DetalleAux_Misc02 = varCtaOrdenante
+                    rowCFD_CPAGO._5_DetalleAux_Misc03 = "BBA830831LJ2"
+                    rowCFD_CPAGO._6_DetalleAux_Misc04 = "0448250278"
+                    rowCFD_CPAGO._7_DetalleAux_Misc05 = "" '"TipoDeCadenaDepago"
+                    rowCFD_CPAGO._8_DetalleAux_Misc06 = "" '"CertificadoDePago"
+                    rowCFD_CPAGO._9_DetalleAux_Misc07 = "" '"CadenaDePago"
+                    rowCFD_CPAGO._10_DetalleAux_Misc08 = "" '"SelloDePago"
+                    rowCFD_CPAGO._11_DetalleAux_Misc09 = ""
+                    rowCFD_CPAGO._12_DetalleAux_Misc10 = ""
+                    rowCFD_CPAGO._13_DetalleAux_Misc11 = ""
+
+                    rowCFD_CPAGO._18_DetalleAux_Misc16 = ""
+                    rowCFD_CPAGO._19_DetalleAux_Folio = rowCFDI_ENC._1_Folio
+                    rowCFD_CPAGO._20_DetalleAux_Serie = rowCFDI_ENC._27_Serie_Comprobante
+
+                    dsCFDI_CPAGO.CFDI_ComplementoPago.Rows.Add(rowCFD_CPAGO)
+                    taCFDI_CPAGO.Update(dsCFDI_CPAGO.CFDI_ComplementoPago)
+
+                    pbTodos.Visible = True
+                    pbTodos.Value = 1
+                    pbTodos.Step = 1
+                    pbTodos.Maximum = dsComplemento.Vw_ChequesDetalle.Rows.Count + 1
+
+
+                    rowCFD_CPAGO = dsCFDI_CPAGO.CFDI_ComplementoPago.NewCFDI_ComplementoPagoRow()
+
+                        rowCFD_CPAGO._1_DetalleAux_Tipo = "CPG"
+                        rowCFD_CPAGO._2_DetalleAux_DescTipo = "DoctoRelacionado"
+                        rowCFD_CPAGO._3_DetalleAux_Misc01 = "HD"
+                        rowCFD_CPAGO._4_DetalleAux_Misc02 = "00000000-0000-0000-0000-000000000000" 'rowsD4.UUID 'dgvUUIDRelacionados.Item("dgUUID", row.Index).Value
+                        rowCFD_CPAGO._5_DetalleAux_Misc03 = ""  'dgvUUIDRelacionados.Item("dgSerie", row.Index).Value
+                        rowCFD_CPAGO._6_DetalleAux_Misc04 = ""  'dgvUUIDRelacionados.Item("dgFolio", row.Index).Value
+                    rowCFD_CPAGO._7_DetalleAux_Misc05 = "MXN" 'validaNull(leeXML(rowsD4.CFDOriginal, "Moneda")) 'dgvUUIDRelacionados.Item("dgMoneda", row.Index).Value
+                    rowCFD_CPAGO._8_DetalleAux_Misc06 = ""
+
+
+                    rowCFD_CPAGO._9_DetalleAux_Misc07 = "PPD" 'validaNull(leeXML(rowsD4.CFDOriginal, "MetodoPago"))
+
+                    rowCFD_CPAGO._10_DetalleAux_Misc08 = "1" 'taCFDI_CPAGO.ObtieneNoPago(rowCFD_CPAGO._4_DetalleAux_Misc02)
+
+                        rowCFD_CPAGO._11_DetalleAux_Misc09 = varMontoDePago
+                        rowCFD_CPAGO._12_DetalleAux_Misc10 = varMontoDePago
+                        rowCFD_CPAGO._13_DetalleAux_Misc11 = CDec(rowCFD_CPAGO._11_DetalleAux_Misc09 - varMontoDePago)
+
+                        rowCFD_CPAGO._18_DetalleAux_Misc16 = ""
+                            rowCFD_CPAGO._19_DetalleAux_Folio = rowCFDI_ENC._1_Folio
+                            rowCFD_CPAGO._20_DetalleAux_Serie = rowCFDI_ENC._27_Serie_Comprobante
+
+                            dsCFDI_CPAGO.CFDI_ComplementoPago.Rows.Add(rowCFD_CPAGO)
+                            taCFDI_CPAGO.Update(dsCFDI_CPAGO.CFDI_ComplementoPago)
+
+                        ' Next
+                        pbTodos.Value += 1
+                        pbTodos.Visible = False
+                        'Next
+                        taConsecutivo.ActAProcesado(rowCFDI_ENC._27_Serie_Comprobante, rowCFDI_ENC._1_Folio, varCheque, rowCFDI_ENC._43_RFC_Receptor)
+                End If
+            Else
+                    Dim valor As Boolean = Me.dgvComplementos.Item("ProcesadoDataGridViewCheckBoxColumn", i).Value
                 If Me.dgvComplementos.Item("ProcesadoDataGridViewCheckBoxColumn", i).Value = True Then
                     varFechaDePago = dgvComplementos.Item("FechaPagoDataGridViewTextBoxColumn", i).Value
                     varFormaDePago = dgvComplementos.Item("FormaDePagoPDataGridViewTextBoxColumn", i).Value
